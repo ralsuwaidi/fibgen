c9c1ac69fe806df35d50dd913ac1a1e0b55f0190
rashed <x.suwaidi@gmail.com>
2021-08-07 09:13:07 +0400

    add download route and spinner

--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -1,1 +1,11 @@
 // place any jQuery/helper plugins in here, instead of separate, slower script files.
+$(document).ready(function () {
+    $("#btnFetch").click(function () {
+        // disable button
+        $(this).prop("disabled", true);
+        // add spinner to button
+        $(this).html(
+            `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>`
+        );
+    });
+});
--- fibgen/user/views.py
+++ fibgen/user/views.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 """User views."""
 from flask import Blueprint, current_app, render_template, request
+from flask.helpers import url_for
 from flask_login import login_required
+from werkzeug.utils import redirect
 
 from fibgen.books import scrape
 
@@ -9,17 +11,28 @@ from .forms import LibgenForm
 
 blueprint = Blueprint("user", __name__, url_prefix="/users", static_folder="../static")
 
+books = []
+
 
 @blueprint.route("/", methods=["GET", "POST"])
 @login_required
 def members():
     """List members."""
+    global books
     form = LibgenForm(request.form)
     books = []
     if request.method == "POST":
         books = scrape(form.book.data)
-        current_app.logger.info(books[0])
+        current_app.logger.info(books[0].links[0])
 
     return render_template(
         "users/members.html", title="Search book", form=form, books=books
     )
+
+
+@blueprint.route("/download/<int:book_no>")
+@login_required
+def download(book_no):
+    global books
+    current_app.logger.info(f"downloading book {books[book_no]}")
+    return redirect(url_for(".members"))
